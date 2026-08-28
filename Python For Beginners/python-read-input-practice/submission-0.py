244115ee66d9f3a10cde24c259cc2a636e7a14d4
Wade Cumins <wcumins@gmail.com>
2026-08-27 21:04:48 -0700

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    lst = input().split(',')
+    return int(lst[0]) + int(lst[1])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
